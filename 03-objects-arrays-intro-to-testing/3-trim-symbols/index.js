f08d2a74f6ae51893e447fe2f57082cf53d8c032
alexx-gsm <alexx-gsm@yandex.ru>
2021-10-01 20:21:35 +0700

    add trimSymbols function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  if (!size) {
+    return string;
+  }
+
+  let originChar;
+  let counter;
+
+  return string.split('').reduce((result, char) => {
+    if (originChar !== char) {
+      originChar = char;
+      counter = size - 1;
+
+      return result + char;
+    }
+
+    if (counter) {
+      --counter;
+      return result + char;
+    }
+
+    return result;
+  }, '');
 }
