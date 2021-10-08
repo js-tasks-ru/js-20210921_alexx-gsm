78096da0b227a738fdc9675bb6d334a70819a63c
alexx-gsm <alexx-gsm@yandex.ru>
2021-10-08 23:20:38 +0700

    remove prev HW

--- 02-javascript-data-types/2-pick/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * pick - Creates an object composed of the picked object properties:
- * @param {object} obj - the source object
- * @param {...string} fields - the properties paths to pick
- * @returns {object} - returns the new object
- */
-export const pick = (obj, ...fields) => {
-  return fields.reduce((result, next) => Object.assign(result, {[next]: obj[next]}), {});
-};
