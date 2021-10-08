1db4a0bcf39775dc53cd1e1adc07159271452500
alexx-gsm <alexx-gsm@yandex.ru>
2021-10-08 20:08:52 +0700

    add Notification Component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,68 @@
 export default class NotificationMessage {
+  message;
+  duration;
+  type;
 
+  constructor(message, {duration, type} = {}) {
+    this.message = message;
+    this.duration = duration || 1000;
+    this.type = type || 'success';
+
+    this.render();
+
+    this.show = this.show.bind(this);
+    this.destroy = this.destroy.bind(this);
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.renderNotification();
+    this.element = wrapper.firstElementChild;
+  }
+
+  renderNotification() {
+    return `
+      <div class="${this.getClassNames()}" style="${this.getDuration()}">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>`;
+  }
+
+  getClassNames() {
+    return `notification ${this.type}`;
+  }
+
+  getDuration() {
+    const durationInSeconds = this.duration / 1000;
+    return `--value:${durationInSeconds}s`;
+  }
+
+  show(parentNode) {
+    if (window.notificationTimer) {
+      this.destroy();
+    }
+
+    if (parentNode) {
+      parentNode.append(this.element);
+    } else {
+      document.body.append(this.element);
+    }
+
+    window.notificationTimer = setTimeout(() => this.destroy(), this.duration);
+  }
+
+  remove() {
+    clearTimeout(window.notificationTimer);
+    document.querySelector('.notification')?.remove();
+  }
+
+  destroy() {
+    this.remove();
+    document.querySelector('.notification')?.remove();
+  }
 }
